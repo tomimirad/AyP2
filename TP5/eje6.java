24dc6bbcd7a88153b4f09689853cee7595e7a893
tomimirad <tomimiranda1403@gmail.com>
2024-09-12 12:58:25 -0300

    olvide subir cabios

--- TP5/eje6.java
+++ TP5/eje6.java
@@ -3,14 +3,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 // Se lee información que indica la cantidad de hectáreas sembradas cada día del mes 
 // de enero. Informe los días en que se sembró menos que el promedio del mes. 
+//para sacar el promedio es la suma de todas hectareas sembradas de los dias del mes de enero divido por la cantidad de dias que tiene el mes de enero (31)
 public class eje6 {
     public static void main(String[] args) {
         int[] enero = new int[32];
+        double hec_promedio = 0;
+        double res_promedio = 0;
         Scanner lector = new Scanner(System.in);
         for (int i = 0; i < enero.length; i++) {
-            System.out.println("ingrese la cantidad de hectareas sembradas:");
-            enero[i] = lector.nextInt();    
+            System.out.println("ingrese la cantidad de hectareas sembradas en el dia: " + (i+1));
+            enero[i] = lector.nextInt();
         }
-        // System.out.println(Arrays.toString(enero));
+        for (int i = 0; i < enero.length; i++) {
+            hec_promedio+= enero[i];    
+        }
+        res_promedio = hec_promedio/31;
+        System.out.println("el promedio: " + res_promedio);
+        for (int i = 0; i < enero.length; i++) {
+            if (enero[i] < res_promedio) {
+                System.out.println("dia "+(i+1)+": "+enero[i] +" hectareas" );
+            }
+        }
     }
 }
--- TP5/eje7.java
+++ TP5/eje7.java
@@ -1,9 +1,32 @@
 package TP5;
+import java.util.Arrays;
+import java.util.Scanner;
 // Leer un vector que representa la temperatura de cada día del mes de junio. Visualizar
 // por pantalla la temperatura más baja, la más alta y los días en que se produjeron
 // respectivamente. 
 public class eje7 {
   public static void main(String[] args) {
-    String[] vectorDias =  {"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+    int[] junio = new int[30];
+    Scanner lector = new Scanner(System.in);
+    int max = junio[0];
+    int min = junio[0];
+    for (int i = 0; i < junio.length; i++) {
+      // System.out.println("ingrese la temperatura del dia: "+(i+1));
+      // junio[i] = lector.nextInt();
+      junio[i] = (int)(50*Math.random());
+    }
+
+    for (int i = 1; i < junio.length; i++) {
+        if (junio[i] > max) {
+          max = junio[i];
+        }
+        if (junio[i] < min) {
+          min = junio[i];
+          System.out.println("hola" + min);
+        }
+    }
+    System.out.println(Arrays.toString(junio));
+    System.out.println("temperatura alta: "+ max);
+    System.out.println("temperatura baja "+ min);
   }
 }
